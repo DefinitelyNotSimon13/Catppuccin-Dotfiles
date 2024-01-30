041f42a1182d7a5d9a031a7e9329f5f682ecd490
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-01-30 14:42:32 +0100

    Remove cache - why was this here?

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ddcacc", "#080D15" },
-	[SchemeSel] = { "#ddcacc", "#63535A" },
-	[SchemeOut] = { "#ddcacc", "#AD9296" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#080D15";
-static const char* selfgcolor   = "#ddcacc";
-static const char* normbgcolor  = "#865B3C";
-static const char* normfgcolor  = "#ddcacc";
-static const char* urgbgcolor   = "#63535A";
-static const char* urgfgcolor   = "#ddcacc";
